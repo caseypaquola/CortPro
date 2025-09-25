9e425ea9989ed8467dc28f8c71ce7935be69c875
caseypaquola <34452058+caseypaquola@users.noreply.github.com>
2025-09-25 10:22:23 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,77 +16,34 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'The Big Brain Warp'
-copyright = '2020*, MICA'
-author = 'Casey Paquola, Boris Bernhardt'
-
+project = 'CortPro'
+author = 'Casey Paquola'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['sphinx_tabs.tabs', 
-              'sphinx.ext.autodoc',
-              'sphinx.ext.autosectionlabel', 
-              'sphinx.ext.autosummary',
-              #'sphinx.ext.doctest',
-              #'sphinx.ext.intersphinx',
-              #sphinx.ext.mathjax',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.viewcode',
-              #'sphinxarg.ext',
-              ]
+extensions = [
+    'sphinx_rtd_theme',   # main ReadTheDocs theme
+    # 'myst_parser',       # enable if you want Markdown support
+    # 'sphinx.ext.autodoc', # enable if you want autodoc for Python code
+]
 
-autosummary_generate = True
-autodoc_default_options = {'members': True, 'inherited-members': True}
-numpydoc_show_class_members = False
-autoclass_content = "class"
-
-# The master toctree document.
-master_doc = 'index'
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+templates_path = ['_templates']
 exclude_patterns = []
-
-highlight_language ='none'
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = { 'style_nav_header_background': '#5d4bb7'}
-
-# The name of the Pygments (syntax highlighting) style to use.
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-pygments_style = 'bigbrainwarplexer.bigbrainwarpLexerStyle'
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
-html_css_files = ['css/bigbrainwarp.css', 
-                  'css/bigbrainwarp_nomaxwidth.css']
+# -- Custom sidebar/logo options (optional) ----------------------------------
 
-# add custom files that are stored in _static
-def setup(app):
-   app.add_css_file('css/bigbrainwarp_tabs.css')
+# You can add a logo if you have one:
+html_logo = "_static/cortpro_logo.png"
 
-# add logo
-html_logo = "warp2.png"
+# Use wide sidebar for navigation
 html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
+    "navigation_depth": 3,
+    "collapse_navigation": False,
+    "style_nav_header_background": "#4e799e",  # matches your blue accent
 }
